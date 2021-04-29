e46ee013d6f565a6d3a132bf1ebd5b86b0817a89
Thilina Ekanayaka <tekanayaka@accelaero.com>
2021-04-29 14:51:46 +0530

    graphql integration

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const app = express();
+const { graphqlHTTP } = require('express-graphql');
+const { buildSchema } = require('graphql');
 
 const port = process.env.PORT || 5000;
 
@@ -15,6 +17,29 @@ const productRoutes = require('./routes/product.routes')
 
 app.use('/api/v1/products', productRoutes)
 
+//-- graphql logic starts here --
+
+const schema = buildSchema(`
+  type Query {
+    hello: String
+  }
+`);
+
+const root = {
+    hello: () => {
+        return 'Hello world!';
+    },
+};
+
+app.use('/graphql', graphqlHTTP({
+    schema: schema,
+    rootValue: root,
+    graphiql: true,
+}));
+
+//-- graphql logic ends here --
+
 app.listen(port, () => {
     console.log(`Server is listening on port ${port}`);
+    console.log(`GraphQL API server at http://localhost:5000/graphql`);
 });
