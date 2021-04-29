80412bdf1d838d065306430fac83c714ecdd449a
Thilina Ekanayaka <tekanayaka@accelaero.com>
2021-04-29 15:00:27 +0530

    added comments to server.js

--- server.js
+++ server.js
@@ -3,26 +3,26 @@ const app = express();
 const { graphqlHTTP } = require('express-graphql');
 const { buildSchema } = require('graphql');
 
+// server config
 const port = process.env.PORT || 5000;
-
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
-
 app.get('/', (req, res) => {
     res.send("Product Catalog App");
 });
 
+// products route specification
 const productRoutes = require('./routes/product.routes')
-
 app.use('/api/v1/products', productRoutes)
+
 
 //-- graphql logic starts here --
 
 const schema = buildSchema(`
-  type Query {
-    hello: String
-  }
+    type Query {
+        hello: String
+    }
 `);
 
 const root = {
@@ -39,6 +39,8 @@ app.use('/graphql', graphqlHTTP({
 
 //-- graphql logic ends here --
 
+
+// server listening on port 5000
 app.listen(port, () => {
     console.log(`Server is listening on port ${port}`);
     console.log(`GraphQL API server at http://localhost:5000/graphql`);
